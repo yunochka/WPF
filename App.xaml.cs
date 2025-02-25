81af234b3db94d429e4720bad84a533d5220b563
yunochka <164052642+yunochka@users.noreply.github.com>
2025-02-25 20:17:08 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF12._1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Запуск одной копии приложения
+        System.Threading.Mutex mutex;
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool createdNew;
+            string mutName = "Приложение";
+            mutex = new System.Threading.Mutex(true, mutName, out createdNew);
+            if (!createdNew)
+            {
+                this.Shutdown();
+            }
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                MessageBox.Show(window.ToString());
+            }
+        }
+    }
+}
