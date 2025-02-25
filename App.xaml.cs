5a6d95aec56cf5fe2314a03678c887dd176d0f91
yunochka <164052642+yunochka@users.noreply.github.com>
2025-02-25 20:12:23 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+public partial class App : Application
+{
+    //Запуск одной копии приложения
+    System.Threading.Mutex mutex;
+    private void Button1_Click(object sender, RoutedEventArgs e)
+    {
+        foreach (Window window in Application.Current.Windows)
+        {
+            MessageBox.Show(window.ToString());
+        }
+    }
+}
