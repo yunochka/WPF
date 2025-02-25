032e783f0970504d3efea6d90f48de62689b5981
yunochka <164052642+yunochka@users.noreply.github.com>
2025-02-25 23:05:30 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14._7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class PhoneRepository
+    {
+        private ObservableCollection<Phone> phones;
+
+        public PhoneRepository()
+        {
+            phones = new ObservableCollection<Phone>
+        {
+            new Phone {Id=1, Title="iPhone 6S", Company="Apple" },
+            new Phone {Id=2, Title="Lumia 950", Company="Microsoft" },
+            new Phone {Id=3, Title="Nexus 5X", Company="Google" },
+            new Phone {Id=4, Title="Galaxy S6", Company="Samsung"}
+        };
+        }
+        public ObservableCollection<Phone> GetPhones()
+        {
+            return phones;
+        }
+    }
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } // модель телефона
+        public string Company { get; set; } // производитель
+    }
+}
