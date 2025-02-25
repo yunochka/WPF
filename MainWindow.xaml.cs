0995d5c8838dfc37c90299392c44d0b63650b136
yunochka <164052642+yunochka@users.noreply.github.com>
2025-02-25 22:59:31 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> phones;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            phones = new List<string> { "iPhone 6S Plus", "Nexus 6P", "Galaxy S7 Edge" };
+            phonesList.ItemsSource = phones;
+        }
+    }
+}
