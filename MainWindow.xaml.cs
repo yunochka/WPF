14a36c85db8754d8e0a726f73a9838147f67ee50
yunochka <164052642+yunochka@users.noreply.github.com>
2025-02-25 18:23:45 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() // Конструктор главного окна
+        {
+            InitializeComponent(); // Создание и настройка привязки данных (Data Binding)
+            // Этот код реализует привязку между TextBox и TextBlock
+
+
+            Binding binding = new Binding();// Создание объекта привязки
+                                            // Настройка параметров привязки:
+
+            binding.ElementName = "myTextBox"; // элемент-источник
+            binding.Path = new PropertyPath("Text"); // свойство элемента-источника
+            myTextBlock.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+        }
+    }
+}
